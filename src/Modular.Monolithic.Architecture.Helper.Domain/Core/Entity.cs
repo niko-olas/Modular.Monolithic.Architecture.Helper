90eaf791ee59d6f183a026cff9082922c165121a
Nicholas Gueli <gueli.nicholas@outlook.it>
2024-07-06 23:27:06 +0200

    mod entity

--- src/Modular.Monolithic.Architecture.Helper.Domain/Core/Entity.cs
+++ src/Modular.Monolithic.Architecture.Helper.Domain/Core/Entity.cs
@@ -9,7 +9,7 @@ namespace Modular.Monolithic.Architecture.Helper.Domain.Core
     /// <summary>
     /// Represents the base class that all entities derive from.
     /// </summary>
-    public abstract class Entity : IEquatable<Entity>
+    public abstract class Entity
     {
 
         private List<IDomainEvent> _domainEvents;
@@ -17,20 +17,7 @@ namespace Modular.Monolithic.Architecture.Helper.Domain.Core
         /// <summary>
         /// Domain events occurred.
         /// </summary>
-        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents?.AsReadOnly();
-        /// <summary>
-        /// Gets or sets the entity identifier.
-        /// </summary>
-        public Guid Id { get; private set; }
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Entity"/> class.
-        /// </summary>
-        /// <param name="id">The entity identifier.</param>
-        protected Entity(Guid id)
-            : this()
-        {
-            Id = id;
-        }
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents?.AsReadOnly();   
         /// <summary>
         /// Initializes a new instance of the <see cref="Entity"/> class.
         /// </summary>
@@ -76,60 +63,5 @@ namespace Modular.Monolithic.Architecture.Helper.Domain.Core
                 throw new BusinessRuleValidationException(rule);
             }
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="left"></param>
-        /// <param name="right"></param>
-        /// <returns></returns>
-        public static bool operator ==(Entity left, Entity right)
-        {
-            if (Object.Equals(left, null))
-                return (Object.Equals(right, null)) ? true : false;
-            else
-                return left.Equals(right);
-        }
-
-        public static bool operator !=(Entity left, Entity right)
-        {
-            return !(left == right);
-        }
-
-        /// <inheritdoc />
-        public bool IsTransient()
-        {
-            return this.Id == default;
-        }
-        /// <inheritdoc />
-        public bool Equals(Entity other)
-        {
-            if (other is null)
-            {
-                return false;
-            }
-
-            return ReferenceEquals(this, other) || Id == other.Id;
-        }
-        /// <inheritdoc />
-        public override bool Equals(object obj)
-        {
-            if (obj == null || !(obj is Entity))
-                return false;
-
-            if (Object.ReferenceEquals(this, obj))
-                return true;
-
-            if (this.GetType() != obj.GetType())
-                return false;
-
-            Entity item = (Entity)obj;
-
-            if (item.IsTransient() || this.IsTransient())
-                return false;
-            else
-                return item.Id == this.Id;
-        }
-        /// <inheritdoc />
-        public override int GetHashCode() => this.Id.GetHashCode() ^ 31;
     }
 }
