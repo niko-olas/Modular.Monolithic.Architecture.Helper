9f7461daeb55d504c2dab6e664c9b46834ce9ff9
Nicholas Gueli <gueli.nicholas@outlook.it>
2024-08-06 22:45:13 +0200

    Add protected cotr

--- src/Modular.Monolithic.Architecture.Helper.Domain/Events/DomainEvent.cs
+++ src/Modular.Monolithic.Architecture.Helper.Domain/Events/DomainEvent.cs
@@ -5,7 +5,7 @@ public class DomainEvent : IDomainEvent
     public Guid Id { get; }
     public DateTime OccurredOn { get; }
 
-    public DomainEvent()
+    protected DomainEvent()
     {
         Id = Guid.NewGuid();
         OccurredOn = DateTime.UtcNow;
