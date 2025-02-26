b158a68a146c996f5c3585c0b0abf53e5f5dc13c
Nicholas Gueli <gueli.nicholas@outlook.it>
2025-02-26 21:34:24 +0100

    Update DomainException

--- src/Modular.Monolithic.Architecture.Helper.Domain/Exceptions/DomainException.cs
+++ src/Modular.Monolithic.Architecture.Helper.Domain/Exceptions/DomainException.cs
@@ -1,4 +1,7 @@
-﻿using Modular.Monolithic.Architecture.Helper.Domain.Errors;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Modular.Monolithic.Architecture.Helper.Domain.Errors;
 
 namespace Modular.Monolithic.Architecture.Helper.Domain.Exceptions;
 
@@ -8,17 +11,20 @@ namespace Modular.Monolithic.Architecture.Helper.Domain.Exceptions;
 public class DomainException : Exception
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="DomainException"/> class.
-    /// </summary>
-    /// <param name="error">The error containing the information about what happened.</param>
-    public DomainException(Error error)
-        : base(error.Message)
-    {
-        Error = error;
-    }
-
-    /// <summary>
     /// Gets the error.
     /// </summary>
     public Error Error { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DomainException"/> class.
+    /// </summary>
+    /// <param name="error">The error containing the information about what happened.</param>
+    public DomainException(Error error) : base(error?.Message ?? Error.None.Message) => Error = error ?? Error.None;
+
+    public DomainException() => Error = Error.None;
+
+    public DomainException(string message) : base(message) => Error = new Error(message, message);
+
+    public DomainException(string message, Exception innerException) : base(message, innerException) => Error = new Error(message, message);
+
 }
