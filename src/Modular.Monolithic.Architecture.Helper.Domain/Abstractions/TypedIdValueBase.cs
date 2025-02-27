ce8e6180fd492618ed23ac1a0c9cf58be97e21e2
Nicholas Gueli <gueli.nicholas@outlook.it>
2025-02-27 21:36:16 +0100

    Change Value to Id

--- src/Modular.Monolithic.Architecture.Helper.Domain/Abstractions/TypedIdValueBase.cs
+++ src/Modular.Monolithic.Architecture.Helper.Domain/Abstractions/TypedIdValueBase.cs
@@ -1,4 +1,7 @@
-﻿namespace Modular.Monolithic.Architecture.Helper.Domain.Abstractions;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Modular.Monolithic.Architecture.Helper.Domain.Abstractions;
 
 /// <summary>
 /// Represents the id value entities.
@@ -8,7 +11,7 @@ public abstract class TypedIdValueBase : IEquatable<TypedIdValueBase>
     /// <summary>
     /// Gets or sets the entity identifier.
     /// </summary>
-    public Guid Value { get; private set; }
+    public Guid Id { get; private set; }
 
     protected TypedIdValueBase(Guid value)
     {
@@ -17,7 +20,7 @@ public abstract class TypedIdValueBase : IEquatable<TypedIdValueBase>
             throw new InvalidOperationException("Id value cannot be empty!");
         }
 
-        Value = value;
+        Id = value;
     }
 
     /// <summary>
@@ -26,35 +29,17 @@ public abstract class TypedIdValueBase : IEquatable<TypedIdValueBase>
     /// <param name="left"></param>
     /// <param name="right"></param>
     /// <returns></returns>
-    public static bool operator ==(TypedIdValueBase left, TypedIdValueBase right)
-    {
-        if (Object.Equals(left, null))
-        {
-            return Object.Equals(right, null);
-        }
-        else
-        {
-            return left.Equals(right);
-        }
-    }
+    public static bool operator ==(TypedIdValueBase left, TypedIdValueBase right) => Object.Equals(left, null) ? Object.Equals(right, null) : left.Equals(right);
 
     public static bool operator !=(TypedIdValueBase left, TypedIdValueBase right) => !(left == right);
 
     /// <inheritdoc />
-    public bool IsTransient() => Value == default;
+    public bool IsTransient() => Id == default;
 
     /// <inheritdoc />
-    public override bool Equals(object obj)
-    {
-        if (obj is null)
-        {
-            return false;
-        }
-
-        return obj is TypedIdValueBase other && Equals(other);
-    }
+    public override bool Equals(object obj) => obj is not null && obj is TypedIdValueBase other && Equals(other);
     // <inheritdoc />
-    public bool Equals(TypedIdValueBase other) => Value == other?.Value;
+    public bool Equals(TypedIdValueBase other) => Id == other?.Id;
     /// <inheritdoc />
-    public override int GetHashCode() => Value.GetHashCode();
+    public override int GetHashCode() => Id.GetHashCode();
 }
